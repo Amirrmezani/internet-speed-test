7df875bea2669a3a56a66c469e5b48b7d95efc01
AmirRamezanzadeh <142434947+Amirrmezani@users.noreply.github.com>
2023-10-29 14:06:03 +0330

    python

---
+++ speedtest.py
@@ -0,0 +1,8 @@
+# internet Speed test With Pyhton 
+
+import speedtest
+speed = speedtest.Speedtest()
+download_speed = speed.download()
+upload_speed = speed.upload()
+print(f'The download speed is {download_speed}')
+print(f'The uplaod speed is {upload_speed}')
